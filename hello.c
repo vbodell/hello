a86ca50c9eea6ee5239c4babd07870595aa7712f
Victor Bodell <vbodell@kth.se>
2017-11-27 12:49:04 -0800

    Update w/ error check

--- hello.c
+++ hello.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+#define HELLO_LEN 14
+
+#define FAIL 1
 #define OK 0
 
 int main(){
-  printf("Hello, world!\n");
+  int status;
+
+  status = printf("Hello, world!\n");
+  
+  if(status != HELLO_LEN){
+    fprintf(stderr, "Printing failed!?\n");
+    return FAIL;
+  }
 
   return OK;
 }
